1d39d1a6863bbc1439e9a7be07f4d5610cf79f09
Karol002 <karol.wojcik.002@gmail.com>
2022-08-13 23:13:22 +0200

    Adding tests for class collection

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -20,6 +20,29 @@ public class CollectionTestSuite {
         System.out.println("Test Case: end");
     }
 
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Tests begin");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("Test finished");
+    }
+
+    @DisplayName("Check that the empty list is maintained")
+
+    @Test
+    void testOddNumbersExterminatorEmptyList() {
+        OddNumbersExterminator exterminator = new OddNumbersExterminator();
+
+        //When
+        boolean isEmpty = exterminator.getEvenNumbers().isEmpty();
+
+        //Then
+        Assertions.assertEquals(isEmpty,true);
+    }
+
     @DisplayName("Check that the full list is maintained")
 
     @Test
