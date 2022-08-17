e31cee125b4904a055b66dcc705fad8e6d8d9247
Karol002 <karol.wojcik.002@gmail.com>
2022-08-17 11:20:38 +0200

    7.1 - Function programing LAMBDA

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/OldMain.java
@@ -0,0 +1,31 @@
+package com.kodilla.stream;
+
+public class OldMain {
+
+    /*
+    ------------------ IMPORTANT INFORMATION AND TIPS FOR MODULE 7.1 -----------------------
+        SaySomething saySomething = new SaySomething();
+        saySomething.say();
+
+        Processor processor1 = new Processor();
+        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
+        processor1.execute(executeSaySomething);
+
+        Processor processor = new Processor();
+        processor.execute(() -> System.out.println("This is an example text."));
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionCalculator::divideAByB);
+        */
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,30 @@
+package com.kodilla.stream;
+
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.beautifier.PoemDecorator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to module 7 - Stream");
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("Decorate",(text) -> text = "ABC" + text + "ABC");
+        poemBeautifier.beautify("Decorate",(text) -> text += text + text);
+        poemBeautifier.beautify("Decorate",(text) -> text.toUpperCase());
+        poemBeautifier.beautify("Decorate",(text) -> {
+            String resultText = "";
+            String singleText = "";
+            for (int i=0; i<text.length(); i++) {
+                singleText = text.substring(i,i+1);
+                resultText += singleText + singleText;
+            }
+            return resultText.toUpperCase();
+        });
+    }
+}
