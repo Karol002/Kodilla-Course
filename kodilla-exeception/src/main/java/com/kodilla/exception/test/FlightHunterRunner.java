305d7500cb1cc760cf1cabc053a73620a09257ee
Karol002 <karol.wojcik.002@gmail.com>
2022-08-28 18:19:41 +0200

    8.4 - Main upgrade

--- kodilla-exeception/src/main/java/com/kodilla/exception/test/FlightHunterRunner.java
+++ kodilla-exeception/src/main/java/com/kodilla/exception/test/FlightHunterRunner.java
@@ -2,47 +2,28 @@ package com.kodilla.exception.test;
 
 public class FlightHunterRunner {
 
+    public static void checkFlight(FlightHunter flightHunter, Flight flight) {
+        try {
+            if (flightHunter.findFlight(flight)) {
+                System.out.println("Flight exist");
+            } else {
+                System.out.println("Flight doesn't exist");
+            }
+        } catch (RouteNotFoundException e) {
+            System.out.println("Airport doesn't exist");
+        } finally {
+            System.out.println("Searching done! \n");
+        }
+    }
     public static void main(String[] args) {
         FlightHunter flightHunter = new FlightHunter();
         Flight existFlight = new Flight("Wrocław", "Gdańsk");
         Flight unExistFlight = new Flight("Wrocław", "Warszawa");
         Flight unExistAirport = new Flight("Wrocław", "Opole");
 
+        checkFlight(flightHunter, existFlight);
+        checkFlight(flightHunter, unExistFlight);
+        checkFlight(flightHunter, unExistAirport);
 
-        try {
-            if (flightHunter.findFlight(existFlight)) {
-                System.out.println("Flight exist");
-            } else {
-                System.out.println("Flight doesn't exist");
-            }
-        } catch (RouteNotFoundException e) {
-            System.out.println("Airport doesn't exist");
-        } finally {
-            System.out.println("Searching done! \n");
-        }
-
-        try {
-            if (flightHunter.findFlight(unExistFlight)) {
-                System.out.println("Flight exist");
-            } else {
-                System.out.println("Flight doesn't exist");
-            }
-        } catch (RouteNotFoundException e) {
-            System.out.println("Airport doesn't exist");
-        } finally {
-            System.out.println("Searching done! \n");
-        }
-
-        try {
-            if (flightHunter.findFlight(unExistAirport)) {
-                System.out.println("Flight exist");
-            } else {
-                System.out.println("Flight doesn't exist");
-            }
-        } catch (RouteNotFoundException e) {
-            System.out.println("Airport doesn't exist");
-        } finally {
-            System.out.println("Searching done! \n");
-        }
     }
 }
